2c0b52cef2f10ddbef599751d906cbe883e3f350
unknown <dave8git@gmail.com>
2021-10-27 13:40:23 +0200

    Add map function in MenuButton.js #2

--- src/components/MenuButton/MenuButton.js
+++ src/components/MenuButton/MenuButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 //import styles from './App.scss';
 import List from '../List/List.js';
 import Column from '../Column/Column';
-//import {pageContents, listData} from '../../data/dataStore';
+import {pageContents, listData} from '../../data/dataStore';
 
 class MenuButton extends React.Component {
     state = {
@@ -15,16 +15,24 @@ class MenuButton extends React.Component {
         }); 
     }
   render() {
+    
     return (
+        
     <div>
         <button onClick={() => this.handleClick()}>
           <p>MenuButton</p>
         </button>
-        {this.state.visible ? <p>I'm visible</p> : null}
+        {this.state.visible ? 
+
+          //this.state.columns.map((n) => {<ul>n</ul>})
+
+           [...listData][columns].map((n) => <ul>{n}</ul>)
+
+         : null
+
+        }
         {/* <p>{this.state.visible ? 'I\'m visible' : 'false'}</p> */}
         
-        {this.state.columns.map((n) => console.log(<ul>n</ul>))}, 
-
         </div>
     
     )
