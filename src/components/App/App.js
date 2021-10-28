1db8ae66e67d195a463f7935e67771acf541ea0e
unknown <dave8git@gmail.com>
2021-10-28 14:12:26 +0200

    add addList func

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,44 @@ import styles from './App.scss';
 import List from '../List/List.js';
 import {pageContents, listData} from '../../data/dataStore';
 import MenuButton from '../MenuButton/MenuButton';
+import Creator from '../Creator/Creator';
+import {settings} from '../../data/dataStore';
 
 class App extends React.Component {
-  
-  render() {
+    state = {
+      lists: this.props.lists || [],
+    }
+    addList(title){
+      this.setState(state => (
+          {
+              columns: [
+                  ...state.columns,
+                  {
+                      key: state.lists.length ? state.lists[state.lists.length-1].key+1 : 0,
+                      title, 
+                      icon: 'list-alt',
+                      cards: []
+                  }
+              ]
+          }
+      ));
+    } 
+    render() {
     return (
       <main className={styles.component}>
         <MenuButton />
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
         <List {...listData} />
+        <div className={styles.columns}>
+            {this.state.lists.map(({key, ...listProps}) => (
+              <List key={key} {...listProps} />
+            ))
+          }
+          </div>
+        <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={title => this.addList(title)} />
+        </div>
         {/* <List title={['Things to do', <sup key='1'>soon!</sup>]} src={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}>
           <p>I'm planning on doing all these things sooner, rathern tahn later!</p>
         </List> */}
