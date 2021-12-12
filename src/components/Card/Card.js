77f654dd426446821bfbed642060c84e3825d43e
unknown <dave8git@gmail.com>
2021-12-12 17:16:32 +0100

    fix trying to make condition work

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,16 +3,18 @@ import styles from './Card.scss';
 import PropTypes from 'prop-types';
 
 const Card = props => {
-  const {title} = props;
+  const {title, icon} = props;
   return (
     <section className={styles.component}>
       {title}
+      {icon}
     </section>
   );
 };
  
 Card.propTypes = {
   title: PropTypes.string,
+  icon: PropTypes.string,
 };
 
 export default Card;
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -4,12 +4,17 @@ import Card from '../Card/Card';
 import Column from '../Column/Column'; 
 
 const SearchResults = props => {
-  const {searchString, cards} = props;
+  const {searchString, cards, title, icon} = props;
   console.log('cards', cards);
   return (
     <section>
       <h3>{searchString}</h3>
-      <Column cards={cards} />
+
+      if(cards) {
+        <Column cards={cards} title={title} icon={icon} />
+      } else {
+        'brak takiej karty'
+      };
 
       {/* {cards.map(cardData => (
         <Card key={cardData.id} {...cardData} />
@@ -21,6 +26,8 @@ const SearchResults = props => {
 SearchResults.propTypes = {
   searchString: PropTypes.string,
   cards: PropTypes.array,
+  title: PropTypes.string,
+  icon: PropTypes.string, 
 }; 
 //
 export default SearchResults;
